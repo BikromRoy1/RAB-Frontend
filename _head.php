d50fd45f66f2659e622e2fdc5b8f56192113992a
Hafizur Rahman <coderpickbd@gmail.com>
2023-11-23 13:03:09 +0600

    news section done

--- _head.php
+++ _head.php
@@ -8,19 +8,14 @@
 
   <!-- Favicons -->
   <link href="assets/img/favicon-32-pixel.png" alt="র‌্যাপিড অ্যাকশন ব্যাটালিয়ন (র‌্যাব)" rel="icon" />
-  <link rel="apple-touch-icon" sizes="48x48" href="assets/img/favicon-48-pixel.png"
-    alt="র‌্যাপিড অ্যাকশন ব্যাটালিয়ন (র‌্যাব) | Icon 1" />
-  <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicon-72-pixel.png"
-    alt="র‌্যাপিড অ্যাকশন ব্যাটালিয়ন (র‌্যাব) | Icon 2" />
-  <link rel="apple-touch-icon" sizes="96x96" href="assets/img/favicon-96-pixel.png"
-    alt="র‌্যাপিড অ্যাকশন ব্যাটালিয়ন (র‌্যাব) | Icon 3" />
+  <link rel="apple-touch-icon" sizes="48x48" href="assets/img/favicon-48-pixel.png" alt="র‌্যাপিড অ্যাকশন ব্যাটালিয়ন (র‌্যাব) | Icon 1" />
+  <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favicon-72-pixel.png" alt="র‌্যাপিড অ্যাকশন ব্যাটালিয়ন (র‌্যাব) | Icon 2" />
+  <link rel="apple-touch-icon" sizes="96x96" href="assets/img/favicon-96-pixel.png" alt="র‌্যাপিড অ্যাকশন ব্যাটালিয়ন (র‌্যাব) | Icon 3" />
 
   <!-- Google Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link
-    href="https://fonts.googleapis.com/css2?family=Noto+Sans+Bengali:wght@100;200;300;400;500;600;700;800;900&display=swap"
-    rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Bengali:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
 
   <!-- Vendor CSS Files -->
   <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
@@ -29,7 +24,8 @@
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
 
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
-
+  <!-- page css -->
+  <link rel="stylesheet" href="assets/css/news.css">
   <!-- Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet" />
 </head>
